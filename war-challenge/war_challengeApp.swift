6058b79128b7f154f6558ad40bbfba88982bd0f8
Sooni Mohammed <sooni.mohaammed@gmail.com>
2021-12-25 17:47:03 -0500

    Initial Commit

---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by Sooni Mohammed on 2021-12-25.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
