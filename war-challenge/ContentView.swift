ed11f045d103955fff44ff6fdd5530fcf7de4522
Sooni Mohammed <sooni.mohaammed@gmail.com>
2021-12-25 19:34:23 -0500

    Card switch logic

--- war-challenge/ContentView.swift
+++ war-challenge/ContentView.swift
@@ -8,6 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var playerCard = "card5"
+    @State private var cpuCard = "card9"
+    @State private var playerScore = 0
+    @State private var cpuScore = 0
+    
     var body: some View {
         ZStack {
             Image("background")
@@ -23,11 +29,11 @@ struct ContentView: View {
                 HStack {
                     Spacer()
                     
-                    Image("card2")
+                    Image(playerCard)
                     
                     Spacer()
                     
-                    Image("card3")
+                    Image(cpuCard)
                     
                     Spacer()
                 }
@@ -35,7 +41,15 @@ struct ContentView: View {
                 Spacer()
                 
                 Button {
-                    print("Deal")
+                    
+                    let playerRand = Int.random(in: 2...14)
+                    let cpuRand = Int.random(in: 2...14)
+                    
+                    playerCard = "card" + String(playerRand)
+                    cpuCard = "card" + String(cpuRand)
+                    
+                    // playerScore += 1
+                    // cpuScore += 1
                 } label: {
                     Image("dealbutton")
                 }
@@ -50,7 +64,7 @@ struct ContentView: View {
                             .font(.headline)
                             .padding(.bottom, 10.0)
                         
-                        Text("0")
+                        Text(String(playerScore))
                             .font(.largeTitle)
                     }
                     
@@ -61,7 +75,7 @@ struct ContentView: View {
                             .font(.headline)
                             .padding(.bottom, 10.0)
 
-                        Text("0")
+                        Text(String(cpuScore))
                             .font(.largeTitle)
                     }
                     
